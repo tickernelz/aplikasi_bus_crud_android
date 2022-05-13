7fc50b37c3a537c6e15360c83bf29f662df5a973
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-14 01:09:58 +0700

    create login view
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

---
+++ app/src/main/java/upr/uas/pedro/ui/login/LoginFragment.java
@@ -0,0 +1,32 @@
+package upr.uas.pedro.ui.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import upr.uas.pedro.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LoginViewModel loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/upr/uas/pedro/ui/login/LoginViewModel.java
@@ -0,0 +1,11 @@
+package upr.uas.pedro.ui.login;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+
+
+    public LoginViewModel() {
+    }
+
+}
