7861c1c90da30c1d42848e7c1ea8e18c7575a3a8
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-13 21:58:54 +0700

    Initial Commit
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

---
+++ app/src/main/java/upr/uas/pedro/ui/home/HomeFragment.java
@@ -0,0 +1,30 @@
+package upr.uas.pedro.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import upr.uas.pedro.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/upr/uas/pedro/ui/home/HomeViewModel.java
@@ -0,0 +1,12 @@
+package upr.uas.pedro.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+
+    public HomeViewModel() {
+
+    }
+
+}
