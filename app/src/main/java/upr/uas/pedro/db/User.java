2909ff19589cf72eba9ffb818734ec461ad21fd6
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-14 12:07:11 +0700

    create User and UserParams class
    - function of User Class is to set id, name, username and password of User
    - function of UserParams Class is to set key parameters table name, id, name, username, password
    
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

---
+++ app/src/main/java/upr/uas/pedro/db/User.java
@@ -0,0 +1,56 @@
+package upr.uas.pedro.db;
+
+public class User {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+
+    public User(int id, String name, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String Password) {
+        password = Password;
+    }
+}
---
+++ app/src/main/java/upr/uas/pedro/db/UserParams.java
@@ -0,0 +1,11 @@
+package upr.uas.pedro.db;
+
+public class UserParams {
+    public static final String TABLE_NAME = "user_params";
+
+    //Keys of our table in db
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+}
