469d4439c7ee1268295924b6ff516538325a1a1b
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-15 11:50:24 +0700

    set penumpang fragment
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

--- app/src/main/java/upr/uas/pedro/ui/penumpang/PenumpangFragment.java
+++ app/src/main/java/upr/uas/pedro/ui/penumpang/PenumpangFragment.java
@@ -16,44 +16,44 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 import upr.uas.pedro.R;
-import upr.uas.pedro.adapter.BusAdapter;
-import upr.uas.pedro.crud.bus.CreateBusActivity;
+import upr.uas.pedro.adapter.PenumpangAdapter;
+import upr.uas.pedro.crud.penumpang.CreatePenumpangActivity;
 import upr.uas.pedro.db.DBHandler;
-import upr.uas.pedro.object.Bus;
+import upr.uas.pedro.object.Penumpang;
 
 public class PenumpangFragment extends Fragment {
 
   DBHandler db;
-  ArrayList<Bus> busList;
-  ListView lvBus;
-  BusAdapter adapter;
-  TextView tvNoBus;
-  FloatingActionButton fabCreateBus;
+  ArrayList<Penumpang> penumpangList;
+  ListView lvPenumpang;
+  PenumpangAdapter adapter;
+  TextView tvNoPenumpang;
+  FloatingActionButton fabCreatePenumpang;
   View mView;
 
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View mView = inflater.inflate(R.layout.fragment_bus, container, false);
+    View mView = inflater.inflate(R.layout.fragment_penumpang, container, false);
     db = new DBHandler(requireContext());
 
-    busList = new ArrayList<>();
-    tvNoBus = mView.findViewById(R.id.viewBus_tv_noBus);
-    lvBus = mView.findViewById(R.id.viewBus_lv_busList);
-    fabCreateBus = mView.findViewById(R.id.viewBus_fab_createBus);
-    busList = db.getBusData();
-    adapter = new BusAdapter(requireContext(), busList, db);
-    lvBus.setAdapter(adapter);
+    penumpangList = new ArrayList<>();
+    tvNoPenumpang = mView.findViewById(R.id.viewPenumpang_tv_noPenumpang);
+    lvPenumpang = mView.findViewById(R.id.viewPenumpang_lv_penumpangList);
+    fabCreatePenumpang = mView.findViewById(R.id.viewPenumpang_fab_createPenumpang);
+    penumpangList = db.getPenumpangData();
+    adapter = new PenumpangAdapter(requireContext(), penumpangList, db);
+    lvPenumpang.setAdapter(adapter);
     adapter.notifyDataSetChanged();
 
-    if (!busList.isEmpty()) {
-      tvNoBus.setVisibility(View.INVISIBLE);
+    if (!penumpangList.isEmpty()) {
+      tvNoPenumpang.setVisibility(View.INVISIBLE);
     } else {
-      tvNoBus.setVisibility(View.VISIBLE);
+      tvNoPenumpang.setVisibility(View.VISIBLE);
     }
 
-    fabCreateBus.setOnClickListener(
+    fabCreatePenumpang.setOnClickListener(
         view -> {
-          Intent i = new Intent(getContext(), CreateBusActivity.class);
+          Intent i = new Intent(getContext(), CreatePenumpangActivity.class);
           startActivity(i);
         });
     return mView;
@@ -62,6 +62,6 @@ public class PenumpangFragment extends Fragment {
   @Override
   public void onDestroyView() {
     super.onDestroyView();
-    upr.uas.pedro.databinding.FragmentBusBinding binding = null;
+    upr.uas.pedro.databinding.FragmentPenumpangBinding binding = null;
   }
 }
